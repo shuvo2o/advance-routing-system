b1d4093d10811aba092a4e0cac87880c0de1294a
shuvo2o <eng.shuvo03@gmail.com>
2025-11-17 10:49:52 +0600

    paralel route

---
+++ src/app/advanced-dashboard/default.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AdvanceDefault = () => {
+    return (
+        <div>
+            <div className='text-lg font-semibold'>
+                Hii, This is Advanced Default 
+            </div>
+        </div>
+    )
+}
+
+export default AdvanceDefault
