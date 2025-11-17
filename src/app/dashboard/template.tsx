6b98052e18733b8211ae1ffb375fbffb5655e5d3
shuvo2o <eng.shuvo03@gmail.com>
2025-11-17 10:57:31 +0600

    conditional rendering done

--- src/app/dashboard/template.tsx
+++ src/app/dashboard/template.tsx
@@ -1,7 +1,12 @@
+import { redirect } from 'next/navigation';
 import React from 'react'
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     console.log("template.tsx rendered...")
+    const isLoggedIn = true;
+    if (!isLoggedIn) {
+        redirect("/login")
+    }
     return (
         <div className='space-y-4'>
             <h1>Dashboard Layout</h1>
